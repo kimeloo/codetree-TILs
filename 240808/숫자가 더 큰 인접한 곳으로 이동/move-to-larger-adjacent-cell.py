a2a46f39bcfdc142639683b1b4f78631dd798efe
kimeloo <149450798+kimeloo@users.noreply.github.com>
2024-08-08 21:30:25 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240808/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -0,0 +1,45 @@
+import sys
+
+def main():
+    outputHandler(inputHandler())
+
+def inputHandler():
+    n, r, c = map(int, sys.stdin.readline().split())
+    numMap = []
+    for _ in range(n):
+        numMap.append(list(map(int, sys.stdin.readline().split())))
+    result = [numMap[r-1][c-1]]
+    nextLoc = (r-1, c-1)
+    while True:
+        nextLoc, nextNum = findNext(nextLoc, numMap)
+        if nextNum==-1:
+            break
+        result.append(nextNum)
+    return result
+
+
+def findNext(currentLoc:tuple, numMap):
+    r, c = currentLoc
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
+
+    for i in range(4):
+        try:
+            nextY = r+dy[i]
+            nextX = c+dx[i]
+            if nextY<0 or nextX<0:
+                raise
+            nextNum = numMap[nextY][nextX]
+        except:
+            continue
+        if nextNum > numMap[c][r]:
+            # print((nextX, nextY), nextNum)
+            return (nextX, nextY), nextNum
+    else:
+        return (), -1
+
+def outputHandler(result):
+    print(*result, sep=" ")
+
+if __name__ == '__main__':
+    main()
