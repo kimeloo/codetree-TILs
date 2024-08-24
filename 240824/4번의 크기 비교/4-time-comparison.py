830a8e5c6e8c864c7e17add8d9874569becfcf21
kimeloo <149450798+kimeloo@users.noreply.github.com>
2024-08-24 21:23:49 +0900

    Title: 4번의 크기 비교, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240824/4번의 크기 비교/4-time-comparison.py
+++ 240824/4번의 크기 비교/4-time-comparison.py
@@ -2,7 +2,5 @@ import sys
 a = int(sys.stdin.readline().strip())
 num = map(int, sys.stdin.readline().split())
 
-result = []
-for n in num:
-    result.append(1 if a>n else 0)
+result = [1 if a>n else 0 for n in num]
 print(*result, sep="\n")
