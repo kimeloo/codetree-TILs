f8f94187968e36c46070082a29d332c04742925b
kimeloo <149450798+kimeloo@users.noreply.github.com>
2024-09-02 20:03:49 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240902/20대의 평균나이/average-age-of-20.py
@@ -0,0 +1,16 @@
+import sys
+
+def main():
+    ages = []
+    while True:
+        age = int(sys.stdin.readline().strip())
+        if age >= 30:
+            break
+        ages.append(age)
+    print('{:.2f}'.format(sum(ages)/len(ages)))
+
+
+
+
+
+main()
