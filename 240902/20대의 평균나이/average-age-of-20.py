e451f39912a4d26f4eb9727b0344b0d3f892bac8
kimeloo <149450798+kimeloo@users.noreply.github.com>
2024-09-02 20:05:51 +0900

    Title: 20대의 평균나이, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240902/20대의 평균나이/average-age-of-20.py
+++ 240902/20대의 평균나이/average-age-of-20.py
@@ -4,7 +4,7 @@ def main():
     ages = []
     while True:
         age = int(sys.stdin.readline().strip())
-        if age >= 30:
+        if (age<20) or (age >= 30):
             break
         ages.append(age)
     print('{:.2f}'.format(float(sum(ages)/len(ages))))
