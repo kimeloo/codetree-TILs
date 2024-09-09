e7deb2deb4298cf5382cd0ef4194690de96f5acc
kimeloo <149450798+kimeloo@users.noreply.github.com>
2024-09-09 13:56:41 +0900

    Title: 그 계절, 그 날, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240909/그 계절, 그 날/that-season-that-day.py
+++ 240909/그 계절, 그 날/that-season-that-day.py
@@ -6,11 +6,11 @@ def inp():
     return y, m, d
 
 def is_leap(year):
-    if year%400:
+    if year%400==0:
         return True
-    elif year%100:
+    elif year%100==0:
         return False
-    elif year%4:
+    elif year%4==0:
         return True
     else:
         return False
