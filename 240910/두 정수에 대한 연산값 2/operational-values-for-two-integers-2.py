543da088bc16a5e45e588dbce781c45f8342db75
kimeloo <149450798+kimeloo@users.noreply.github.com>
2024-09-10 14:02:20 +0900

    Title: 두 정수에 대한 연산값 2, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240910/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
+++ 240910/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -4,8 +4,13 @@ def inp():
     a, b = map(int, sys.stdin.readline().split())
     return a,b
 def cal(a,b):
-    a, b = min(a,b), max(a,b)
-    return a+10, b*2
+    if a>b:
+        a*=2
+        b+=10
+    else:
+        a+=10
+        b*=2
+    return a, b
 
 def main():
     a, b = inp()
